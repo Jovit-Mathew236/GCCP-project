457313ad69c61ea40f7849131d3f166aa47c623b
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-01-04 19:04:14 +0530

    develop: make images ramdom doenloadable

--- index.js
+++ index.js
@@ -1,40 +1,8 @@
-const form = document.querySelector("#uploadForm");
-const responseDiv = document.querySelector("#response");
+const images = ["eight.png", "9.png", "three.png"];
 
-const predictFunction = (event) => {
-    event.preventDefault();
-
-    const fileInput = document.querySelector("#fileInput");
-    const file = fileInput.files[0];
-    const formData = new FormData();
-    formData.append("file", file);
-
-    fetch("https://getprediction-gzrkj7ydfa-uc.a.run.app", {
-        method: "POST",
-        body: formData,
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            responseDiv.innerHTML =
-                "The predicted number is " + JSON.stringify(data).slice(14, 15);
-        });
+const random = Math.floor(Math.random() * images.length);
+console.log(images[random]);
+function changeImage() {
+    document.getElementById("d-img").setAttribute('href', "images/"+images[random])
 }
-
-// form.addEventListener("submit", function (event) {
-//     event.preventDefault();
-
-//     const fileInput = document.querySelector("#fileInput");
-//     const file = fileInput.files[0];
-//     const formData = new FormData();
-//     formData.append("file", file);
-
-//     fetch("https://getprediction-gzrkj7ydfa-uc.a.run.app", {
-//         method: "POST",
-//         body: formData,
-//     })
-//         .then((response) => response.json())
-//         .then((data) => {
-//             responseDiv.innerHTML =
-//                 "The predicted number is " + JSON.stringify(data).slice(14, 15);
-//         });
-// });
+window.onload = changeImage;
