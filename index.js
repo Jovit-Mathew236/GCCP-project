957b3ae31e9cfc4244510566347ed7b1409ab2e9
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-01-06 20:15:40 +0530

    added loading text to button

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const images = ["eight.png", "9.png", "three.png"];
 
 const random = Math.floor(Math.random() * images.length);
-console.log(images[random]);
+// console.log(images[random]);
 function changeImage() {
     document.getElementById("d-img").setAttribute('href', "images/"+images[random])
 }
